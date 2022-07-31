523d887eecc054ed0a03421c69a660b1a681e9e5
ssjoshi89 <106461510+ssjoshi89@users.noreply.github.com>
2022-08-01 04:50:56 +0530

    Create script.sh

---
+++ ansible/script.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+sudo apt update -y
+sudo apt-get install docker-ce -y
+docker --version
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
